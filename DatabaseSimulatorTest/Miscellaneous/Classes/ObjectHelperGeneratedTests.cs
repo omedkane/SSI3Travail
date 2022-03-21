c2048f658b4110959d9c586b835154656ad29d4c
Ibn <omedkane4@gmail.com>
2022-03-21 01:36:29 -0400

    Added ObjectHelperGeneratedTests

---
+++ DatabaseSimulatorTest/Miscellaneous/Classes/ObjectHelperGeneratedTests.cs
@@ -0,0 +1,90 @@
+﻿using DatabaseSimulator.Miscellaneous;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace DatabaseSimulatorTest.Miscellaneous.Classes
+{
+    [TestClass]
+    public class ObjectHelperGeneratedTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        private ObjectHelper CreateObjectHelper()
+        {
+            return new ObjectHelper();
+        }
+
+        [TestMethod]
+        public void GetCommonProperties_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var objectHelper = this.CreateObjectHelper();
+            Type? typeOfSource = null;
+            Type? typeOfTarget = null;
+
+            // Act
+            var result = objectHelper.GetCommonProperties(
+                typeOfSource,
+                typeOfTarget);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod]
+        public void CopyTo_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var objectHelper = this.CreateObjectHelper();
+            TSource source = default(TSource);
+            TTarget target = default(TTarget);
+            Type? typeOfSource = null;
+            Type? typeOfTarget = null;
+            IEnumerable<string>? commonProperties = null;
+
+            // Act
+            var result = objectHelper.CopyTo(
+                source,
+                target,
+                typeOfSource,
+                typeOfTarget,
+                commonProperties);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod]
+        public void Set_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var objectHelper = this.CreateObjectHelper();
+            T target = default(T);
+            string property = null;
+            object value = null;
+
+            // Act
+            objectHelper.Set(
+                target,
+                property,
+                value);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
